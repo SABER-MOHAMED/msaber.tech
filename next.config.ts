072384585b03327642787550fa703fe2b5a569d3
Saber <mohamed.saber@edu.uiz.ac.ma>
2025-11-29 20:06:24 +0100

    enable hero sec

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'preview.redd.it',
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,18 +5,15 @@ export default function Hero() {
   return (
     <section aria-label="About Me" className="hero" id="about">
       <Image
-        // src="/images/mohamed-saber.jpg"
-        src="https://preview.redd.it/post-the-best-cat-memes-you-got-in-the-comments-please-v0-kg82lbnu0ste1.png?width=320&crop=smart&auto=webp&s=d7842b004b6d96b224b2cbb8af63798d1a10b583"
+        src="/images/mohamed-saber.jpg"
         alt="Mohamed Saber"
         className="hero-image"
         width={200}
         height={200}
       />
-      <h2 className="hero-title">
-        Looking for the file, right!!!!! Here you are
-      </h2>
+      <h2 className="hero-title">Mohamed Saber</h2>
       <p className="hero-subtitle">
-        {/* Master's candidate in <span className="highlight">Embedded AI</span>{' '}
+        Master's candidate in <span className="highlight">Embedded AI</span>{' '}
         with 2+ years of professional{' '}
         <span className="highlight">software engineering</span> experience. I'm
         continuously expanding my knowledge in{' '}
@@ -32,20 +29,14 @@ export default function Hero() {
         <span className="highlight">user experience design</span>. I strive to
         understand emerging methods for running AI models efficiently on
         resource-constrained devices, bringing advanced AI concepts closer to
-        practical embedded applications.*/}
+        practical embedded applications.
       </p>
 
       <div className="hero-buttons">
-        <a
-          href="/content/Git_Github.pdf"
-          className="hero-button hero-button-primary"
-        >
-          Git & GitHub Workshop
-        </a>
-        {/* <a href="#contact" className="hero-button hero-button-secondary">
+        <a href="#contact" className="hero-button hero-button-secondary">
           Contact
         </a>
-        <a
+        {/* <a
           href="/content/Mohamed-Saber-CV.pdf"
           className="hero-button hero-button-primary"
         >
